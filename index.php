2c12830daabe67acf117988978f5c783957cd948
kjhgyjhhg <105437955+kjhgyjhhg@users.noreply.github.com>
2023-09-08 21:24:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Show Product</title>
+	<!-- Bootstrap CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php include 'menu.php'; ?>
+	<div class="container">
+		<h3>ทดสอบการสร้างเมนู</h3>
+		<p>การพํฒนาเว็บไซต์ในส่วนของเมนู</p>
+
+		       <div class="slider-frame">
+            <div class="slide-images">
+                    <div class="img-container">
+                        <img src="images/pe1.jpg">
+                    </div>
+                    <div class="img-container">
+                        <img src="images/pe2.jpg">
+                    </div>
+                    <div class="img-container">
+                        <img src="images/pe3.jpg">
+                    </div>
+            </div>
+        </div>
+        </body>
+    </html>
+
+
+
+
+	</div>
+</body>
+</html>
+	
